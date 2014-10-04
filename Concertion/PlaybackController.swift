0b089017009683369cfc369bba5800d2fa6bb234
Joachim Bengtsson <joachimb@gmail.com>
2014-10-04 18:36:03 +0200

    playback control infra

--- Concertion/PlaybackController.swift
+++ Concertion/PlaybackController.swift
@@ -17,6 +17,7 @@ public struct Track {
 }
 
 public class Concertion: NSObject {
+	public var name = RandomConcertionName()
 	public internal(set) var currentTrack: Track?
 	public internal(set) var playing: Bool = false
 	public internal(set) var time: PlaybackTime?
@@ -34,6 +35,23 @@ public class Concertion: NSObject {
 			return now - setAt + offset
 		}
 	}
+	
+	// Broadcast after making changes
+	public func sendWasChangedLocally()
+	{
+		for listener in changedLocallyListeners {
+			listener()
+		}
+	}
+	var changedLocallyListeners : [() -> ()] = []
+	
+	public func sendWasChangedRemotely()
+	{
+		for listener in changedRemotelyListeners {
+			listener()
+		}
+	}
+	var changedRemotelyListeners : [() -> ()] = []
 }
 
 
@@ -60,11 +78,44 @@ public class PlaybackController: NSObject {
 	public func play(track: Track) {
 		player.play(track.streamingURL.absoluteString)
 		self.currentConcertion.currentTrack = track
+		self.currentConcertion.playing = true
+		self.currentConcertion.time = Concertion.PlaybackTime(setAt:NSDate().timeIntervalSince1970, offset:0)
+		self.currentConcertion.sendWasChangedLocally()
+	}
+	
+	public func joinConcertion(concertion: Concertion) {
+		self.currentConcertion.changedRemotelyListeners.removeAll()
+		self.currentConcertion = concertion
+		self.currentConcertion.changedRemotelyListeners.append({
+			[unowned self]
+			(Void) -> Void in
+			self.updateFromConcertion()
+		})
+		self.updateFromConcertion()
 	}
 	
 	// MARK: Private
 	private var player: STKAudioPlayer = STKAudioPlayer()
 	private lazy var obs : FBKVOController = FBKVOController(observer: self, retainObserved: false)
 	
+	private func updateFromConcertion() {
+		if let track = self.currentConcertion.currentTrack {
+			player.play(track.streamingURL.absoluteString)
+			player.seekToTime(self.currentConcertion.time!.currentOffset())
+		} else {
+			self.player.pause()
+		}
+	}
+	
 }
 var _playControllerSingleton : PlaybackController?
+
+extension Array {
+	func randomChoice() -> T {
+		return self[Int(arc4random_uniform(UInt32(self.count)))]
+	}
+}
+
+func RandomConcertionName() -> String {
+	return ["Speedy", "Lounging", "Turbulent", "Fascinating", "Tectonic", "Hallucinatory", "Timid", "Midnight"].randomChoice() + " " + ["Llama", "Ocean", "Tulip", "Thunderstorm", "Blue", "Pancake", "Torus", "Autumn Evening", "Oracle", "Orange", "Face", "Plankton"].randomChoice()
+}
