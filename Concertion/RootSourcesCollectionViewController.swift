de271f8b995422ca65e4e96fd82343b97700b270
Joachim Bengtsson <joachimb@gmail.com>
2014-10-04 11:16:33 +0200

    Extremely rough storyboard

---
+++ Concertion/RootSourcesCollectionViewController.swift
@@ -0,0 +1,108 @@
+//
+//  RootSourcesCollectionViewController.swift
+//  Concertion
+//
+//  Created by Joachim Bengtsson on 2014-10-04.
+//  Copyright (c) 2014 Concertionists. All rights reserved.
+//
+
+import UIKit
+
+
+class RootSourcesCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.clearsSelectionOnViewWillAppear = false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+	
+	// MARK: Data
+	
+	func contentForSection(section: Int) -> [AnyObject]
+	{
+		return [
+			[1, 2, 3],
+			[1, 2, 3, 4, 5],
+			[1, 2, 3, 4]
+		][section];
+	}
+	
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 3
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return contentForSection(section).count + 1
+    }
+	
+    override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView
+	{
+		let view = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "SectionHeader", forIndexPath: indexPath) as UICollectionReusableView
+		let label = view.viewWithTag(1) as UILabel
+		
+		label.text = ["Nearby Channels", "Recommended SR stations", "Spotify music"][indexPath.section]
+		
+		return view
+	}
+
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+		let content = contentForSection(indexPath.section)
+		if indexPath.row == content.count {
+			return collectionView.dequeueReusableCellWithReuseIdentifier("MoreCell", forIndexPath: indexPath) as UICollectionViewCell
+		}
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("FriendCell", forIndexPath: indexPath) as UICollectionViewCell
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
