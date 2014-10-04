986de4c9e67b901bbbbf3f17faab3c0745ac5c0c
Joachim Bengtsson <joachimb@gmail.com>
2014-10-04 14:53:34 +0200

    SR placeholder VCs

--- Concertion/SREpisodesInShowCollectionViewController.swift
+++ Concertion/SREpisodesInShowCollectionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  RootSourcesCollectionViewController.swift
+//  SREpisodesInShowCollectionViewController.swift
 //  Concertion
 //
 //  Created by Joachim Bengtsson on 2014-10-04.
@@ -8,91 +8,45 @@
 
 import UIKit
 
-class RootSourcesCollectionViewController: UICollectionViewController {
-    
-    var stations :SverigesRadio.ChannelList? = nil
+class SREpisodesInShowCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         self.clearsSelectionOnViewWillAppear = false
-        
-        SverigesRadio().channels().onComplete { (value) -> () in
-            self.stations = value;
-            self.collectionView?.reloadData()
-        }
     }
 
-	
+
+    /*
     // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-		//segue.destinationViewController
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
     }
-	
-	// MARK: Data
-	
-	func contentForSection(section: Int) -> [String]
-	{
-        let tracks: Array<String> = (self.stations != nil) ? self.stations!.map({ (channel) -> String in
-             return channel.liveAudioFileURL.absoluteString!
-        }) : ["Empty"]
-
-        return [
-			["One", "Two", "Three"],
-            tracks,
-			["http://sverigesradio.se/topsy/ljudfil/4119397.mp3", "One", "Two", "Three"]
-		][section]
-	}
-	
+    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         //#warning Incomplete method implementation -- Return the number of sections
-        return 3
+        return 1
     }
 
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //#warning Incomplete method implementation -- Return the number of items in the section
-        return contentForSection(section).count + 1
+        return 1
     }
-	
-    override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView
-	{
-		let view = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "SectionHeader", forIndexPath: indexPath) as UICollectionReusableView
-		let label = view.viewWithTag(1) as UILabel
-		
-		label.text = ["Nearby Channels", "Recommended SR stations", "Spotify music"][indexPath.section]
-		
-		return view
-	}
-
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-		let content = contentForSection(indexPath.section)
-		if indexPath.row == content.count {
-			return collectionView.dequeueReusableCellWithReuseIdentifier("MoreCell", forIndexPath: indexPath) as UICollectionViewCell
-		}
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("FriendCell", forIndexPath: indexPath) as UICollectionViewCell
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as UICollectionViewCell
     
         // Configure the cell
     
         return cell
     }
-	
-	override  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
-	{
-		let content = contentForSection(indexPath.section)
-
-		if indexPath.row == content.count {
-			self.performSegueWithIdentifier("showSR", sender: self)
-			return
-		}
-
-		var selection = content[indexPath.row] as String
-		PlaybackController.sharedInstance().play(NSURL(string: selection)!)
-	}
 
     // MARK: UICollectionViewDelegate
 
