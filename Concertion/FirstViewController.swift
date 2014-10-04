dfcb653bdbdf02dbedc1168d11b72ca0b1f089cc
Joachim Bengtsson <joachimb@gmail.com>
2014-10-04 10:09:12 +0200

    Initial Commit

---
+++ Concertion/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Concertion
+//
+//  Created by Joachim Bengtsson on 2014-10-04.
+//  Copyright (c) 2014 Concertionists. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
